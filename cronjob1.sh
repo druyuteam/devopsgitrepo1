4ee16f4a2b18be74752dbd0c75ad1f7d75379238
druyuteam <83181816+druyuteam@users.noreply.github.com>
2021-05-14 22:55:36 +0530

    Update cronjob1.sh

--- cronjob1.sh
+++ cronjob1.sh
@@ -1,1 +1,3 @@
 30 08 14 05 * date /Users/apple/Desktop/gitdemo/cronjobs
+20 06 14 05 * cat /etc/os-release /Users/apple/Desktop/gitdemo/cronjobs
+21 06 14 05 * vi crondemos /Users/apple/Desktop/gitdemo/cronjobs
